8c125549d77b7a5a30ecf4ffce862b071ff17b8f
Sheetal <gajare.sheetal@gmail.com>
2026-03-29 22:57:53 +1100

    created  cllent Navbar

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+import { Container, CssBaseline} from "@mui/material";
+import axios from "axios";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/ActivityDashboard";
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  useEffect(() => {
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+      .then(response => setActivities(response.data))
+  }, [])
+  return (
+    <>
+      <CssBaseline />
+      <NavBar />
+      <Container maxWidth='xl' sx={{ mt: 3 }}>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </>
+  )
+}
+
+export default App
